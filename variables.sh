6c171610645163c1b67ad6a47b3ce4b3dccc1c86
hellochandru <hellochandruu@gmail.com>
2025-12-07 15:15:27 +0530

    Create variables.sh with dialogue and variable usage
    Fix variable declaration and echo statements.

---
+++ variables.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "ramesh :: hi suresh, How are you ?" 
+echo "suresh :: Hello ramesh, i am good, How about you ?"
+echo "ramesh :: i am fine too, how is your work ?
+echo "suresh :: not bad, i am thinking to upgrade to devops."
+
+# 2nd program
+#variable decleration
+PERSON1=ramesh
+PERSON2-suresh
+
+echo "$PERSON1 :: Hi $PERSOPN2, How are you?"
+echo "$PERSON2 :: Hello $PERSON1, I am doing good, How about you?"
+echo "$PERSON1 :: I am fine too, how is your work ?"
+echo "$PERSON2 :: not bad, i am thinking to upgrade to devops"
+
+
+#3 If you want to declare with confidential information, we can use this type of option".
+PERSON1=1$
+PERSON2=2$
+echo "$PERSON1 :: Hi $PERSOPN2, How are you?"
+echo "$PERSON2 :: Hello $PERSON1, I am doing good, How about you?"
+echo "$PERSON1 :: I am fine too, how is your work ?"
+echo "$PERSON2 :: not bad, i am thinking to upgrade to devops"
+
+
+
+
